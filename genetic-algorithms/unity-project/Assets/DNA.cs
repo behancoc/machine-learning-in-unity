36290dfeb95b5a7d41cb7547250df8cabebd8071
bhancock <brandon.e.hancock@icloud.com>
2020-04-14 15:51:31 -0700

    Updates to project

---
+++ genetic-algorithms/unity-project/Assets/DNA.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    //Gene for color
+    public float r;
+    public float g;
+    public float b;
+
+    bool dead = false;
+
+    public float timeToDie = 0;
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+
+    private void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        Debug.Log("Dead At:" + timeToDie);
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ genetic-algorithms/unity-project/Assets/PopulationManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject personPrefab;
+    public int populationSize = 10;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+            GameObject gameObject = Instantiate(personPrefab, position, Quaternion.identity);
+            gameObject.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+            gameObject.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+            gameObject.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+            population.Add(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
