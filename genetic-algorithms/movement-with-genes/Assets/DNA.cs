4be91d4d40fb50a4ca87062af5a72e7aedbef962
bhancock <brandon.e.hancock@icloud.com>
2020-04-23 19:06:28 -0700

    Adds script files that for some reason didn't appear when pushing previous commits

---
+++ genetic-algorithms/movement-with-genes/Assets/DNA.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA
+{
+    List<int> genes = new List<int>();
+    int dnaLength = 0;
+    int maxValues = 0;
+
+    public DNA(int l, int v)
+    {
+        dnaLength = l;
+        maxValues = v;
+        SetRandom();
+    }
+
+    public void SetRandom()
+    {
+        genes.Clear();
+        for (int i = 0; i < dnaLength; i ++)
+        {
+            genes.Add(Random.Range(0, maxValues));
+        }
+    }
+
+    public void setInt(int position, int value)
+    {
+        genes[position] = value;
+    }
+
+    public void combine(DNA d1, DNA d2)
+    {
+        for(int i = 0; i < dnaLength; i ++)
+        {
+            if (i < dnaLength / 2.0)
+            {
+                int c = d1.genes[i];
+                genes[i] = c;
+            }
+            else
+            {
+                int c = d2.genes[i];
+                genes[i] = c;
+            }
+            
+        }
+    }
+
+    public void Mutate()
+    {
+        genes[Random.Range(0, dnaLength)] = Random.Range(0, maxValues);
+    }
+
+    public int GetGene(int position)
+    {
+        return genes[position];
+    }
+
+}
---
+++ genetic-algorithms/movement-with-genes/Assets/PopulationManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject botPrefab;
+    public int populationSize;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trialTime = 5;
+    int generation = 1;
+
+    GUIStyle guiStyle = new GUIStyle();
+
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect (10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
